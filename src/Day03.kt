f491351981e3ac92731b95502cba7dd7a401a84b
dungngminh <63831488+dungngminh@users.noreply.github.com>
2024-12-06 00:09:07 +0700

    day 3 done

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,56 @@
-
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val regex = Regex("""mul\((\d+),(\d+)\)""")
+        var sum = 0
+        for (line in input) {
+            regex.findAll(line).forEach { matchResult ->
+                val (x, y) = matchResult.destructured
+                sum += x.toInt() * y.toInt()
+            }
+        }
+        return sum
     }
+
     fun part2(input: List<String>): Int {
-        return 0
+        val mulRegex = Regex("""mul\((\d+),(\d+)\)""")
+        val doRegex = Regex("""do\(\)""")
+        val dontRegex = Regex("""don't\(\)""")
+        var sum = 0
+        var mulEnabled = true
+
+        for (line in input) {
+            var currentIndex = 0
+            while (currentIndex < line.length) {
+                val mulMatch = mulRegex.find(line, currentIndex)
+                val doMatch = doRegex.find(line, currentIndex)
+                val dontMatch = dontRegex.find(line, currentIndex)
+
+                val nextMatch =
+                    listOfNotNull(mulMatch, doMatch, dontMatch)
+                        .minByOrNull { it.range.first }
+
+                if (nextMatch != null) {
+                    currentIndex = nextMatch.range.last + 1
+                    when (nextMatch.value) {
+                        doMatch?.value -> mulEnabled = true
+                        dontMatch?.value -> mulEnabled = false
+                        mulMatch?.value -> {
+                            if (mulEnabled) {
+                                val (x, y) = mulMatch.destructured
+                                sum += x.toInt() * y.toInt()
+                            }
+                        }
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+        return sum
     }
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 161)
     val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
